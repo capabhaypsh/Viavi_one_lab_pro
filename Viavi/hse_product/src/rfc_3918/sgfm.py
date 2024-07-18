5ca4e46500249585c9ebc78cd2f1ad106107b880
capabhaypsh <abhay02,bluethink@gmail.com>
2024-07-18 16:44:39 +0530

    simple flask code for viavi rfc python files

---
+++ Viavi/hse_product/src/rfc_3918/sgfm.py
@@ -0,0 +1,35 @@
+import logging
+import sys
+sys.path.append('../')
+
+class sgfm:
+    values = {
+        "enable":{
+            "is_relevant":False,
+            "value": "value defined in enum file which in common folder."
+        },
+        "strating_no_of_group":"enum values"
+        }
+
+    def __init__(self):
+        self.sgfm = sgfm()
+
+
+    def validite_connectivity(self):
+        pass
+    
+    def strat(self):
+        pass
+    
+    def restrat(self):
+        pass
+    
+    def stop(self):
+        pass
+    
+    def set(self):
+        pass
+    
+    def subscribe(self):
+        pass
+
---
+++ Viavi/hse_product/src/rfc_3918/tests.py
@@ -0,0 +1,23 @@
+from rfc_3918 import sgfm
+class Tests:
+    def __init__(self):
+        self.sgfm = sgfm()
+
+
+    def validite_connectivity(self):
+        pass
+    
+    def strat(self):
+        pass
+    
+    def restrat(self):
+        pass
+    
+    def stop(self):
+        pass
+    
+    def set(self):
+        pass
+    
+    def subscribe(self):
+        pass
